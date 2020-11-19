a1a1027a2cea5ede57ca696fc16f3c0ca51b9520
kmayur16 <mayur.bk@robosoftin.com>
2020-11-19 20:56:43 +0530

    created pokedex for searching pokemon with both national id and name

--- src/api.js
+++ src/api.js
@@ -1,16 +1,71 @@
-async function fetchPokemons() {
-    let response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100&offset=200');
+import  './api.css'
+import { useState} from "react";
 
-    if (response.ok) {
-        // if HTTP-status is 200-299
-        // get the response body (the method explained below)
-        // let json = await response.json();
-        // console.log(json);
-        return response.json();
-    } else {
-        //alert('HTTP-Error: ' + response.status);
-        throw new Error('HTTP-Error: ' + response.status);
+const Api =() =>{
+    const [pokemon,setPokemon] = useState("");
+    const [data,setData] = useState([]);
+
+    async function fetchPokemonURL() {
+        const array = [];
+        try {
+            const url =`https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+            const result=await fetch(url).then(response=>response.json()).then(datta=>datta);
+            console.log("b",result);
+            array.push(result);
+            setData(array);
+        } catch (error) {
+            console.log(error)
+        }
     }
+    const handleChange= (e)=>{
+        setPokemon(e.target.value);
+    }
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        fetchPokemonURL();
+    }
+    return (
+        <div className="app">
+            <form onSubmit= {handleSubmit}>
+                <label>
+                    <input type="text" placeholder="enter pokemon" onChange={handleChange}></input>
+                </label>
+                <button type="submit" className="btn" onClick={handleSubmit}>search</button>
+                
+            </form>
+            {data.map((d)=>{
+                return(
+                    <div className="container">
+                        <img src={d.sprites["front_default"]} className="img" alt=""></img>
+                        <div className="divTable">
+                        <div>
+                        <div className="Table">
+                            <div div className="Cell">Type</div>
+                            <div className="Cell">{d.types[0].type.name}</div>
+                        </div>
+                        <div className="Table">
+                            <div div className="Cell">Height</div>
+                            <div className="Cell">{d.height}"</div>
+                        </div>
+                        <div className="Table">
+                            <div div className="Cell">Weight</div>
+                            <div className="Cell">{d.weight}lbs</div>
+                        </div>
+                        <div className="Table">
+                            <div div className="Cell">Moves</div>
+                            <div className="Cell">{d.moves[0].move.name}</div>
+                        </div>
+                        <div className="Table">
+                            <div div className="Cell">Ability</div>
+                            <div className="Cell">{d.abilities[0].ability.name}</div>
+                        </div>
+                    </div>
+                    </div>
+                    </div>
+                )
+            })}
+        </div>
+    );
 }
+export default Api;
 
-export { fetchPokemons };
--- src/api.test.js
+++
@@ -1,7 +0,0 @@
-import { fetchPokemons } from './api';
-
-it('fetchPokemons test', () => {
-    return fetchPokemons().then((json) => {
-        expect(json).not.toBeNull();
-    });
-});
